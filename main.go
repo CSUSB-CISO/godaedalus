4773c90750529eaedd54481765fcf4885854cdb6
TonightsBigLoser <ivancallejas96@gmail.com>
2022-10-12 19:34:13 -0700

    small hands + fix indent

--- main.go
+++ main.go
@@ -79,37 +79,37 @@ func main() {
 //possibly just remove these shit services: https://www.cyberciti.biz/tips/linux-security.html
 
 func Telnet() {
-	/*	err := os.Create("telnet")
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
-	url := ("https://raw.githubusercontent.com/CSUSB-CISO/godaedalus/main/Configurations/telnet")
-	
-		trequest, err := http.Get(url)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	
-		tbody, err := ioutil.ReadAll(trequest.Body)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	
-		file2write := []byte(string(tbody))
-	
-		/*what, err := os.Write(file2write)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	*/
-	
-		os.WriteFile("telnet", []byte(file2write), 0644)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	
+/*	err := os.Create("telnet")
+	if err != nil {
+		log.Fatal(err)
 	}
+*/
+url := ("https://raw.githubusercontent.com/CSUSB-CISO/godaedalus/main/Configurations/telnet")
+
+	trequest, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tbody, err := ioutil.ReadAll(trequest.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	file2write := []byte(string(tbody))
+
+	/*what, err := os.Write(file2write)
+	if err != nil {
+		log.Fatalln(err)
+	}
+*/
+
+	os.WriteFile("telnet", []byte(file2write), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
 
 
 func disableCoreDumps() {
